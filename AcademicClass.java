528a8b0d44141d0b356da03ed30c47e89bff31a1
Alex Barkmeier <33204087+abarky24@users.noreply.github.com>
2020-09-27 19:01:18 -0500

    Update AcademicClass.java

--- AcademicClass.java
+++ AcademicClass.java
@@ -1,4 +1,32 @@
 
 public class AcademicClass.java {
+  Private double avgGPA;
+  Private double percentA;
+  Private double percentF;
+  
+  //TODO: write constructor
 
+  protected double getAvgGPA() {
+    
+  }
+  
+  protected double getPercentA() {
+    
+  }
+  
+  protected double percentF() {
+    
+  }
+  
+  protected void setAvgGPA() {
+    
+  }
+  
+  protected void setPercentA() {
+    
+  }
+  
+  protected void setPercentF() {
+    
+  }
 }
