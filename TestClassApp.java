b8d6eed7400ed462f17ba2821096e74e74261376
achakrabor24 <71959953+achakrabor24@users.noreply.github.com>
2020-09-27 19:27:40 -0500

    Add files via upload

---
+++ TestClassApp.java
@@ -0,0 +1,107 @@
+// --== CS400 File Header Information ==--
+// Name: Aritra Chakraborty
+// Email: achakrabor24@wisc.edu
+// Team: FD
+// TA: Abhay
+// Lecturer: Dahl
+// Notes to Grader: <optional extra notes>
+
+
+/**
+ * Tests all functions of app.
+ */
+public class TestClassApp {
+  // Data Wranglers
+  public static boolean testConstructor(){
+    // Call constructor and create new object
+    // Check if objects parameters are equal to what was just set
+    return false;
+  }
+
+  // Front end
+
+  /**
+   * Tests if the menu functions correctly based on input.
+   * @return
+   */
+  public static boolean testMenu(){
+    return false;
+  }
+
+  /**
+   * Tests if the error message correctly displays after an invalid input.
+   * @return
+   */
+  public static boolean testErrorMessage(){
+    return false;
+  }
+
+  // Back end (test the functions)
+
+  /**
+   *
+   * @return
+   */
+  public static boolean testContainsKey(){
+    return false;
+  }
+
+  /**
+   * Checks if size is correct.
+   * @return
+   */
+  public static boolean testSize(){
+    return false;
+  }
+
+  /**
+   * Tests if the put method correctly inserts keys and values.
+   * @return
+   */
+  public static boolean testPut() {
+    return false;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public static boolean testGet(){
+    return false;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public static boolean testRemove(){
+    return false;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public static boolean testClear(){
+    return false;
+  }
+
+
+
+
+
+
+  public static void main(String args[]){
+    System.out.println("1. testConstructor(): "+ testConstructor());
+    System.out.println("2. testErrorMessage(): "+ testErrorMessage());
+    System.out.println("3. testContainsKey(): "+ testContainsKey());
+    System.out.println("4. testSize: "+ testSize());
+    System.out.println("5. testPut(): "+ testPut());
+    System.out.println("6. testGet(): "+ testGet());
+    System.out.println("7. testRemove(): "+ testRemove());
+    System.out.println("8. testClear(): "+ testClear());
+
+
+  }
+
+}
